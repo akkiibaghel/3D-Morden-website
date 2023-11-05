235a5c2e6769a267f91c9fee7b2849736dff9c89
Abhishek kumar <akkiibaghel2@gmail.com>
2023-11-06 00:17:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+Shery.imageEffect("#back",{style: 5, config: {"a":{"value":0.99,"range":[0,30]},"b":{"value":-0.82,"range":[-1,1]},
+"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.1006564200727866},"gooey":{"value":true},
+"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},
+"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.07,
+"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},
+"onMouse":{"value":1},"noise_speed":{"value":0.17,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],
+"_gsap":{"id":3}},"discard_threshold":{"value":0.4,"range":[0,1]},"antialias_threshold":{"value":0.04,"range":[0,0.1]},
+"noise_height":{"value":0.4,"range":[0,2]},"noise_scale":{"value":18.18,"range":[0,100]}}, gooey: true})
